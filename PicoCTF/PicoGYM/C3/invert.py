c6e017146bfb09d7805ff8606a14c63c147563f0
twobad69 <162154372+twobad69@users.noreply.github.com>
2026-02-15 06:12:09 +0100

    C3 WRITEUP

---
+++ PicoCTF/PicoGYM/C3/invert.py
@@ -0,0 +1,19 @@
+import sys
+
+lookup1 = "\n \"#()*+/1:=[]abcdefghijklmnopqrstuvwxyz"
+lookup2 = "ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst"
+
+lookup2_index = {c: i for i, c in enumerate(lookup2)}
+
+ciphertext = "DLSeGAGDgBNJDQJDCFSFnRBIDjgHoDFCFtHDgJpiHtGDmMAQFnRBJKkBAsTMrsPSDDnEFCFtIbEDtDCIbFCFtHTJDKerFldbFObFCFtLBFkBAAAPFnRBJGEkerFlcPgKkImHnIlATJDKbTbFOkdNnsgbnJRMFnRBNAFkBAAAbrcbTKAkOgFpOgFpOpkBAAAAAAAiClFGIPFnRBaKliCgClFGtIBAAAAAAAOgGEkImHnIl"
+
+out = []
+prev = 0
+
+for char in ciphertext:
+    cipher_index = lookup2_index[char]
+    cur = (cipher_index + prev) % 40
+    out.append(lookup1[cur])
+    prev = cur
+
+print("".join(out))
---
+++ PicoCTF/PicoGYM/C3/readme.md
@@ -0,0 +1,5 @@
+use invert script to decrypt the ciphertext
+[alt text](invertion.JPG)
+then use the outputed script to get flag!
+[alt text](c3.JPG)
+which will net us the flag picoCTF{adlib}
